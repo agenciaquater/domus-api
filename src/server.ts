78f826571046d27dfa3c43c6adb7ea4c1a82638b
username <email>
2022-07-29 11:49:30 -0300

    feat: express setup and basic implementation

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('pedrao')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'pedrao' });
+});
+
+app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
