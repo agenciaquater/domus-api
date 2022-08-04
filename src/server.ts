81414865e560cd70f14b6cbf34765ccf352e47a2
username <email>
2022-08-04 11:32:14 -0300

    refactor: add process.env port declaration for heroku

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
+app.listen(process.env.port ?? 3333, () =>
+  console.log('Servidor rodando na porta 3333')
+);
