88db75f67757219a4f1d2ca2ddef2721a281f65b
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-08-08 16:21:33 -0300

    feat:geração senha hash na criação do user

--- src/repositories/users/CreateUserRepository.ts
+++ src/repositories/users/CreateUserRepository.ts
@@ -1,22 +1,34 @@
+import bcrypt from 'bcrypt';
 import { User } from '../../models/User';
 import { client } from '../../services/prisma';
 
 export class CreateUserRepository {
   async create(user: User) {
     try {
-      const createdUser = await client.user.create({
-        data: {
-          cpf: user.cpf,
-          email: user.email,
-          password: user.password,
-          full_name: user.full_name,
-        },
-        select: {
-          email: true,
-          id: true,
-          full_name: true,
-        },
-      });
+      const createdUser = bcrypt.hash(
+        user.password,
+        10,
+        async (err, encrypted) => {
+          if (err) {
+            throw new Error(err.message);
+          }
+          const userWithHashedPassword = await client.user.create({
+            data: {
+              cpf: user.cpf,
+              email: user.email,
+              password: encrypted,
+              full_name: user.full_name,
+            },
+            select: {
+              email: true,
+              id: true,
+              full_name: true,
+            },
+          });
+          return userWithHashedPassword;
+        }
+      );
+
       return createdUser;
     } catch (e) {
       throw new Error(e);
