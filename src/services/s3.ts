72366a6accd06baf0bbc218e1af1844d06a69de4
pedrodahmer <pedrohd1213@gmail.com>
2023-01-26 12:39:19 -0300

    feat: n sei mais

--- src/services/s3.ts
+++ src/services/s3.ts
@@ -1,7 +1,7 @@
 import { GetObjectAclCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from 'dotenv';
-import { MulterFile } from 'src/@types/multer-file';
+import { MulterFile } from '../@types/multer-file';
 
 dotenv.config()
 
