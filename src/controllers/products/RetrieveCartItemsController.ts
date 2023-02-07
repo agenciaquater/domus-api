99895abdde131000aeddcf561175b1962c66ba3b
pedrodahmer <pedrohd1213@gmail.com>
2023-02-07 11:01:41 -0300

    refactor: image urls on retrieve cart

--- src/controllers/products/RetrieveCartItemsController.ts
+++ src/controllers/products/RetrieveCartItemsController.ts
@@ -1,6 +1,8 @@
 import { Product } from '@prisma/client';
 import { Request, Response } from 'express';
+import { Replace } from '../../helpers/replace';
 import { client } from '../../services/prisma';
+import { getS3ImageUrls } from "../../utils/get-s3-image-urls";
 
 interface RequestUnit {
   id: string;
@@ -12,9 +14,14 @@ interface RetrieveCartItemsRequest {
 }
 
 interface CartItem {
-  product: Product
+  product: Replace<Product, {images: Image[]}>
   quantity: number;
   totalPrice: number
+}
+
+interface Image {
+  name: string;
+  url: string
 }
 
 export class RetrieveCartItemsController {
@@ -28,13 +35,29 @@ export class RetrieveCartItemsController {
           const product = await client.product.findFirst({
             where: {
               id: item.id
+            },
+            include: {
+              category: {
+                select: {
+                  id: true,
+                  name: true
+                }
+              }
             }
           })
           if (!product) {
             return
           }
+          const imageUrls = await getS3ImageUrls(product.images)
+          let images: Image[] = []
+          product.images.map((image, index) => {
+            images.push(shapeToObject(image, imageUrls[index]))
+          })
           cartItems.push({
-            product,
+            product: {
+              ...product,
+              images,
+            },
             quantity: item.quantity,
             totalPrice: item.quantity * (product.sale_price ?? product.price)
           })
@@ -46,5 +69,12 @@ export class RetrieveCartItemsController {
         .status(500)
         .json({ message: 'Internal Server Error', error: error.message });
     }
+  }
+}
+
+function shapeToObject(imageName: string, imageUrl: string) {
+  return {
+    name: imageName,
+    url: imageUrl
   }
 }
