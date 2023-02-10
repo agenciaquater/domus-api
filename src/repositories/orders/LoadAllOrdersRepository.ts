d228907835b1431e069d2cc2b8dd39464c19fd96
pedrodahmer <pedrohd1213@gmail.com>
2023-02-10 15:48:23 -0300

    feat: load orders with formatted items

--- src/repositories/orders/LoadAllOrdersRepository.ts
+++ src/repositories/orders/LoadAllOrdersRepository.ts
@@ -16,10 +16,50 @@ export class LoadAllOrdersRepository {
       const prismaOrders = await client.order.findMany({
         include: defaultOrderInclude
       })
+      if (!prismaOrders) {
+        return
+      }
       let orders: CompleteOrder[] = []
+      for (let i = 0; i < prismaOrders.length; i++) {
+        let items: ModifiedProduct[] = []
+        const prismaItems = prismaOrders[i].items
+        let currentItem: Product = {} as Product;
+        for (let j = 0; j < prismaItems.length; j++) {
+          if (currentItem.id !== prismaItems[j]) {
+            const product = await client.product.findFirst({
+              where: {
+                id: prismaItems[j]
+              }
+            })
+            if (!product) {
+              return
+            }
+            const quantity = getQuantityOfItems(prismaItems, prismaItems[j])
+            items.push({
+              ...product,
+              quantity,
+            })
+            currentItem = product
+          }
+        }
+        orders.push({
+          ...prismaOrders[i],
+          items
+        })
+      }
       return orders
     } catch (error) {
       throw new Error(error.message)
     }
   }
+}
+
+function getQuantityOfItems(group: string[], item: string): number {
+  let quantity = 0
+  group.map(groupItem => {
+    if (groupItem === item) {
+      quantity += 1
+    }
+  })
+  return quantity
 }
--- src/repositories/orders/LoadOrdersByUserRepository.ts
+++ src/repositories/orders/LoadOrdersByUserRepository.ts
@@ -1,18 +1,68 @@
+import { Order, Product } from "@prisma/client"
 import { client } from "../../services/prisma"
 import { defaultOrderInclude } from "./defaultOrderReturn"
+
+interface ModifiedProduct extends Product{
+  quantity: number
+}
+
+interface CompleteOrder extends Omit<Order, 'items'> {
+  items: ModifiedProduct[]
+}
 
 export class LoadOrdersByUserRepository {
   async load(userId: string) {
     try {
-      const orders = await client.order.findMany({
+      const prismaOrders = await client.order.findMany({
         where: {
           userId,
         },
         include: defaultOrderInclude
       })
+      if (!prismaOrders) {
+        return
+      }
+      let orders: CompleteOrder[] = []
+      for (let i = 0; i < prismaOrders.length; i++) {
+        let items: ModifiedProduct[] = []
+        const prismaItems = prismaOrders[i].items
+        let currentItem: Product = {} as Product;
+        for (let j = 0; j < prismaItems.length; j++) {
+          if (currentItem.id !== prismaItems[j]) {
+            const product = await client.product.findFirst({
+              where: {
+                id: prismaItems[j]
+              }
+            })
+            if (!product) {
+              return
+            }
+            const quantity = getQuantityOfItems(prismaItems, prismaItems[j])
+            items.push({
+              ...product,
+              quantity,
+            })
+            currentItem = product
+          }
+        }
+        orders.push({
+          ...prismaOrders[i],
+          items
+        })
+      }
       return orders
     } catch (error) {
       throw new Error(error.message)
     }
   }
+}
+
+function getQuantityOfItems(group: string[], item: string): number {
+  let quantity = 0
+  group.map(groupItem => {
+    if (groupItem === item) {
+      quantity += 1
+    }
+  })
+  return quantity
 }
