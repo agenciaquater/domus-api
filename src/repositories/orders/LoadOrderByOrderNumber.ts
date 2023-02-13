bc21277c4cbbf14c3252e08c7d9183f70c63b1bc
pedrodahmer <pedrohd1213@gmail.com>
2023-02-13 15:02:17 -0300

    refactor: load order by order number returns items

--- src/repositories/orders/LoadOrderByOrderNumber.ts
+++ src/repositories/orders/LoadOrderByOrderNumber.ts
@@ -1,4 +1,9 @@
+import { Product } from "@prisma/client"
 import { client } from "../../services/prisma"
+
+interface ModifiedProduct extends Product{
+  quantity: number
+}
 
 export class LoadOrderByOrderNumberRepository {
   async load(orderNumber: string) {
@@ -20,9 +25,45 @@ export class LoadOrderByOrderNumberRepository {
           }
         }
       })
-      return order
+      if (!order) {
+        return
+      }
+      let items: ModifiedProduct[] = []
+      let currentItem: string = ''
+      for (let i = 0; i < order.items.length; i++) {
+        if (currentItem !== order.items[i]) {
+          const product = await client.product.findFirst({
+            where: {
+              id: order.items[i]
+            }
+          })
+          if (!product) {
+            return
+          }
+          const quantity = getQuantityOfItems(order.items, order.items[i])
+          items.push({
+            ...product,
+            quantity,
+          })
+          currentItem = order.items[i]
+        }
+      }
+      return {
+        ...order,
+        items
+      }
     } catch (error) {
       throw new Error(error.message)
     }
   }
+}
+
+function getQuantityOfItems(group: string[], item: string): number {
+  let quantity = 0
+  group.map(groupItem => {
+    if (groupItem === item) {
+      quantity += 1
+    }
+  })
+  return quantity
 }
