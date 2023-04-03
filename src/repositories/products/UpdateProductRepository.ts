aefcc9fe676717df72a12db51dace14c82d7dbd8
agenciaquater <agencia@quater.rs>
2023-04-03 11:15:03 -0300

    feat: update product atualiza product e images

--- src/repositories/products/UpdateProductRepository.ts
+++ src/repositories/products/UpdateProductRepository.ts
@@ -1,20 +1,62 @@
-import { client } from "../../services/prisma";
+import { client } from '../../services/prisma';
+
+interface ConnectObject {
+  id: string;
+}
 
 export class UpdateProductRepository {
   async update(id: string, data: any) {
+    const newMatches: ConnectObject[] = [];
+    const oldMatches: ConnectObject[] = [];
+
+    data.matches.map((match: string) => {
+      newMatches.push({
+        id: match,
+      });
+    });
+
+    data.oldMatches.map((match: string) => {
+      oldMatches.push({
+        id: match,
+      });
+    });
+
     try {
       const product = await client.product.update({
         where: {
-          id: id
+          id: id,
         },
         data: {
-          ...data
-        }
-      })
+          matches: {
+            disconnect: oldMatches,
+            connect: newMatches,
+          },
+          name: data.name,
+          price: data.price,
+          sale_price: data.sale_price,
+          description: data.description,
+          volume: data.volume,
+          recipient: data.recipient,
+          store: data.store,
+          grape: data.grape,
+          color: data.color,
+          scent: data.scent,
+          harmonization: data.harmonization,
+          images: data.images,
+          category: {
+            connect: {
+              id: data.categoryId,
+            },
+          },
+        },
+        include: {
+          matches: true,
+        },
+      });
 
-      return product
+      return product;
     } catch (error) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
   }
 }
