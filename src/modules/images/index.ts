fba89541ced03442f47c73a801f3036c55e1b918
agenciaquater <agencia@quater.rs>
2023-04-03 14:59:19 -0300

    feat: sem middleware na rota de exlcuir imagens

--- src/modules/images/index.ts
+++ src/modules/images/index.ts
@@ -1,18 +1,24 @@
 import express from 'express';
 import multer from 'multer';
 
-import { DeleteFilesController, UploadFilesController } from '../../controllers/images';
+import {
+  DeleteFilesController,
+  UploadFilesController,
+} from '../../controllers/images';
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-const uploadImagesController = new UploadFilesController()
-const deleteImagesController = new DeleteFilesController()
+const uploadImagesController = new UploadFilesController();
+const deleteImagesController = new DeleteFilesController();
 
 const imagesModule = express.Router();
 
-imagesModule.post('/media/images/upload', upload.array("images"), uploadImagesController.handle)
-imagesModule.post('/media/images/delete', upload.array("images"), deleteImagesController.handle)
+imagesModule.post(
+  '/media/images/upload',
+  upload.array('images'),
+  uploadImagesController.handle
+);
+imagesModule.post('/media/images/delete', deleteImagesController.handle);
 
 export { imagesModule };
-
